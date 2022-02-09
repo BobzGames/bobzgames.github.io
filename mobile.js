4a8198f59491be655c31397b891847f0821268cd
BobzGames <69918803+BobzGames@users.noreply.github.com>
2022-02-09 09:32:23 -0500

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,1 +1,1 @@
-if( /Android|webOS|iPhone|Safari|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {window.location.replace("https://bobzgames.github.io/m/index.html");}
+if( /Safari/i.test(navigator.userAgent) ) {window.location.replace("https://bobzgames.github.io/m/index.html");}
